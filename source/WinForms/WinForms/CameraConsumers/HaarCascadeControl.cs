53fcae689ef3ec4b5f14efa33e2f316bd72c7fe4
neutmute <neutmute.email>
2014-09-06 23:08:47 +1000

    add mouth detector

--- source/WinForms/WinForms/CameraConsumers/HaarCascadeControl.cs
+++ source/WinForms/WinForms/CameraConsumers/HaarCascadeControl.cs
@@ -23,8 +23,9 @@ namespace PiCamCV.WinForms.CameraConsumers
         {
             InitializeComponent();
 
-            var xmlContent = Resource.GetStringFromEmbedded(typeof(CascadeDetector).Assembly, "PiCamCV.Common.haarcascades.haarcascade_lego_batman2.xml");
+            //var xmlContent = Resource.GetStringFromEmbedded(typeof(CascadeDetector).Assembly, "PiCamCV.Common.haarcascades.haarcascade_lego_batman2.xml");
             //var xmlContent =File.ReadAllText(@"C:\CodeOther\PiCamCV\source\WinForms\WinForms\CameraConsumers\FaceDetection\haarcascade_frontalface_default.xml");
+            var xmlContent = File.ReadAllText(@"C:\CodeOther\PiCamCV\source\PiCamCV.Common\haarcascades\haarcascade_castrillon_mouth.xml");
             _detector = new CascadeDetector(xmlContent);
         }
 
