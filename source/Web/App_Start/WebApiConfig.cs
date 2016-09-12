7176cf58c11d453800687c9e6910298a0dc6016b
neutmute <neutmute@users.noreply.github.com>
2016-09-13 08:06:00 +1000

    remove unused

--- source/Web/App_Start/WebApiConfig.cs
+++ source/Web/App_Start/WebApiConfig.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
 namespace PiCam.Web
@@ -15,7 +14,6 @@ namespace PiCam.Web
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
