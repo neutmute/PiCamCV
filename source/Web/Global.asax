e6fe5d6ab6ac0a82dbd2981fcf19f9c5796aba34
neutmute <neutmute@users.noreply.github.com>
2016-09-12 21:46:29 +1000

    seed web project

---
+++ source/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PiCam.Web.WebApiApplication" Language="C#" %>
---
+++ source/Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PiCam.Web.Startup))]
+
+namespace PiCam.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
