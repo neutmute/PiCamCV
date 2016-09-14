0d4004fd6e257692594bee574cd2a94078d33cd9
neutmute <neutmute@users.noreply.github.com>
2016-09-14 18:35:02 +1000

    npm/grunt

--- source/Web/Controllers/HomeController.cs
+++ source/Web/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace PiCam.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
---
+++ source/Web/Gruntfile.js
@@ -0,0 +1,38 @@
+﻿/// <binding AfterBuild='postBuild' Clean='clean' ProjectOpened='projectOpen' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: {
+            js: ["typescript/**/*.js", "assets/app-ts.*"]
+        },
+
+        ts: {
+            default: {
+                src: ["typescript/**/*.ts", "!node_modules/**"],
+                out: 'assets/app-ts.js'
+            }
+        },
+        watch: {
+            typescript: {
+                files: ['typescript/**/*.ts'],
+                tasks: ['ts']
+            }
+        },
+        typings: {
+            install: {}
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-typings");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.registerTask("default", ["ts"]);
+    
+    grunt.registerTask('postBuild', []); // shouldnt need to compile Typescript since watch will do it
+    grunt.registerTask('projectOpen', ['typings', 'ts', 'watch']);
+};
