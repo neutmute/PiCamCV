f3c21c2f7f84ab57266789b3178425cdc9a5195b
neutmute <neutmute@users.noreply.github.com>
2016-10-18 21:31:56 +1100

    asp.net core colon syntax

--- source/Web.Client/Config.cs
+++ source/Web.Client/Config.cs
@@ -8,8 +8,12 @@ namespace Web.Client
 {
     /// <summary>
     /// # WINDOWS POWERSHELL. Restart visual studio afterward
-    /// [Environment]::SetEnvironmentVariable("PiCamCv:Web:Server", "localhost", "User") 
-    /// [Environment]::SetEnvironmentVariable("PiCamCv:Web:Port", "4091", "User") 
+    /// [Environment]::SetEnvironmentVariable("PiCamCv__Web__Server", "localhost", "User") 
+    /// [Environment]::SetEnvironmentVariable("PiCamCv__Web__Port", "4091", "User") 
+    /// 
+    /// # RASPBERRY PI BASH. Add this to ~/.profile
+    /// export PiCamCv__Web__Server=192.168.1.3
+    /// export PiCamCv__Web__Port=80
     /// </summary>
     public static class Config
     {
@@ -20,8 +24,8 @@ namespace Web.Client
         
         static Config()
         {
-            ServerHost = Environment.GetEnvironmentVariable("PiCamCv:Web:Server", EnvironmentVariableTarget.User);
-            ServerPort = Environment.GetEnvironmentVariable("PiCamCv:Web:Port", EnvironmentVariableTarget.User);
+            ServerHost = Environment.GetEnvironmentVariable("PiCamCv__Web__Server", EnvironmentVariableTarget.User);
+            ServerPort = Environment.GetEnvironmentVariable("PiCamCv__Web__Port", EnvironmentVariableTarget.User);
         }
     }
 }
