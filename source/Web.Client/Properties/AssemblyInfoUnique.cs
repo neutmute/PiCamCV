7526d86b87ec189c150e8a50cd3d6565b52b6b8e
neutmute <neutmute@users.noreply.github.com>
2016-10-19 06:42:21 +1100

    metadata

---
+++ source/Web.Client/Properties/AssemblyInfoUnique.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PiCamCV.Web.Client")]
+[assembly: AssemblyDescription("Client for PiCam.Web")]
+[assembly: AssemblyProduct("PiCamCV.Web.Client")]
+
